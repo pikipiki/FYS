de8b3e947d2eb973566c11418e293767f279e577
pikipiki <boubouki@gmail.com>
2016-12-08 14:43:45 +0100

    loginform working as expected

--- src/app/components/auth/auth-form/auth-form.controller.js
+++ src/app/components/auth/auth-form/auth-form.controller.js
@@ -1,42 +1,18 @@
 var AuthFormController = function(AuthService){
 
   this.createUser = function() {
-    // this.message = null
-    // this.error = null
 
-    console.log(1)
-
-    // AuthService.$createUserWithEmailAndPassword(this.email, this.password)
-    //   .then(function(firebaseUser) {
-    //     this.message = "User created with uid: " + firebaseUser.uid
-    //     console.log(2)
-    //   }).catch(function(error) {
-    //     console.log(3)
-    //     this.error = error
-    //   })
-
-    AuthService.$createUserWithEmailAndPassword(this.email, this.password)
-      .then(function(firebaseUser) {
-        this.message = "User created with uid: " + firebaseUser.uid
-        console.log(2)
-      },function(error) {
-        console.log(3)
-        this.error = error
-      })
-
+    return AuthService
+      .$createUserWithEmailAndPassword(this.user.email, this.user.password)
+      .then(
+        () => console.log('this works')    
+      )
+      .catch(
+        (error) => console.log(error)
+      )
 
   }
 
-  this.deleteUser = function() {
-    this.message = null
-    this.error = null
-
-    AuthService.$deleteUser().then(function() {
-      this.message = "User deleted"
-    }).catch(function(error) {
-      this.error = error
-    })
-  }
 }
 
 
